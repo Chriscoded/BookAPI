bb049b1376823fa0c9d4799ddc9c60a1ce4f5863
chriscoded <amaemechris@gmail.com>
2022-04-19 06:26:15 -1200

    Add project files.

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository booksRepository;
+
+        public BooksController(IBookRepository booksRepository)
+        {
+            this.booksRepository = booksRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await booksRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Book> GetBooks(int id)
+        {
+            return await booksRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook([FromBody] Book book)
+        {
+            var newBook = booksRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Book>> PutBook(int id, [FromBody] Book books)
+        {
+            if(id != books.Id)
+            {
+                return BadRequest();
+            }
+
+            await booksRepository.Update(books);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Book>> DeleteBook(int id)
+        {
+            var bookToDelete = await booksRepository.Get(id);
+            if (bookToDelete == null)
+            {
+                return NotFound();
+            }
+
+            await booksRepository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
+    }
+}
---
+++ BookAPI/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext context;
+
+        public BookRepository(BookContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Book> Create(Book book)
+        {
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+
+            return book;
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+          return await context.Books.ToListAsync();
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            return await context.Books.FindAsync(id);
+        }
+
+        public async Task Update(Book book)
+        {
+            context.Entry(book).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var BookToDelete = await context.Books.FindAsync(id);
+            context.Books.Remove(BookToDelete);
+            await context.SaveChangesAsync();
+        }
+    }
+}
